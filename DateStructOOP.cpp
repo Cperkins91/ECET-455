36d90e7c4f835d978587e79bb2759edd6b106690
Cperkins91 <cperkins91@gmail.com>
2021-02-12 09:37:06 -0500

    Add files via upload

---
+++ DateStructOOP.cpp
@@ -0,0 +1,40 @@
+//
+// Created by cperk on 2/11/2021.
+//
+/*
+ * Start with the date structure in Exercise 5 in Chapter 4 and transform it into a date
+class. Its member data should consist of three ints: month, day, and year. It should also
+have two member functions: getdate(), which allows the user to enter a date in
+12/31/02 format, and showdate(), which displays the date.
+ */
+
+#include <iostream>
+using namespace std;
+
+class date {
+public:
+    void setdate(int d ,int m ,int y){
+        day = d;
+        month = m;
+        year = y;
+    }
+    void getdate() const{
+        cout << "The date you input was: " << day << "/" << month << "/" << year << endl;}
+private:
+    int day;
+    int month;
+    int year;
+};
+
+int main()
+{
+    class date d1;
+    int d,m,y;
+    char dummy;
+    cout << "Please enter the date in format DD/MM/YYYY: " << endl;
+    std::cin >> d >> dummy >> m >> dummy >> y;
+    d1.setdate(d , m, y);
+    d1.getdate();
+}
+
+
